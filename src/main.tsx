147227c1b3bfcc46d43fab6c83ba85bdb38577a0
Slaik <viacheslavkolmyk@gmail.com>
2024-11-04 18:08:04 +0300

    add i-18 library and transnale help page

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './components/App';
+import './i18n';
 
 createRoot(document.getElementById('root')!).render(<App />);
--- src/pages/HelpPage/HelpText/index.tsx
+++ src/pages/HelpPage/HelpText/index.tsx
@@ -11,6 +11,7 @@ import {
   Accordion,
 } from '@mantine/core';
 import { FC } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
 
 import {
   iphoneImages,
@@ -25,18 +26,20 @@ import styles from './styles.module.scss';
 const { Item, Panel, Control } = Accordion;
 
 const HelpText: FC = () => {
+  const { t } = useTranslation('help');
+
   return (
     <Container className={styles.helpPage}>
       <Group>
         <NavLink
-          label="О проекте"
+          label={t('nav.aboutProject')}
           href="#about-project"
           className={styles.navLink}
         />
-        <NavLink label="Использование" className={styles.navLink}>
+        <NavLink label={t('nav.usage')} className={styles.navLink}>
           {NAV_LINKS.map((el, i) => (
             <NavLink
-              label={el}
+              label={t(`nav.${el}`)}
               href={`#${el}`}
               className={styles.navLink}
               key={i}
@@ -48,57 +51,58 @@ const HelpText: FC = () => {
       <Divider my="lg" />
 
       <section id="about-project">
-        <Title order={2}>О проекте</Title>
+        <Title order={2}>{t('sections.aboutProject.title')}</Title>
         <Text mt="sm">
-          Мы не отслеживаем ваш трафик и не собираем какую-либо информацию 😉
-          <br /> <br />
-          Если что-то пошло не так или вы заметили не правильную работу
-          приложения обращаться к
-          <Anchor className={styles.link} href="https://t.me/slaik31">
-            Slaik
-          </Anchor>
-          или
-          <Anchor mr={0} className={styles.link} href="https://t.me/sqrrtm">
-            Rayya
-          </Anchor>
-          .
+          <Trans
+            i18nKey="help:sections.aboutProject.description"
+            components={[
+              <Anchor
+                href="https://t.me/slaik31"
+                target="_blank"
+                rel="noopener noreferrer"
+                key={0}
+              />,
+              <Anchor
+                href="https://t.me/sqrrtm"
+                target="_blank"
+                rel="noopener noreferrer"
+                key={1}
+              />,
+            ]}
+          />
         </Text>
       </section>
 
       <Divider my="lg" />
 
       <section>
-        <Title order={2}>Использование</Title>
-        <Text mt="sm">
-          Ниже описаны способы установки на различные платформы
-        </Text>
+        <Title order={2}>{t('sections.usage.title')}</Title>
+        <Text mt="sm">{t('sections.usage.description')}</Text>
       </section>
 
       <section id="Iphone">
-        <Title order={3}>Iphone</Title>
+        <Title order={3}>{t('sections.iphone.title')}</Title>
         <Text mt="sm">
-          <span className={styles.textBlock}>
-            Скачать Приложение
-            <Anchor
-              className={styles.link}
-              href="https://apps.apple.com/us/app/v2raytun/id6476628951"
-              target="_blank"
-              mr={0}
-            >
-              v2RayTun
-            </Anchor>
-            . После чего скопировать конфиг и добавить его в приложение. Правый
-            верхний укол нажать + после чего{' '}
-            <em>Добавить из буфера → Разрешить вставку → Подключиться.</em>
-          </span>
-          <br />
-          Также у данного приложения существует большой перечень настроек для
-          выборочного применения VPN к приложениям, сайтам или регионам в
-          разделе <em>Службы.</em>
+          <Trans
+            i18nKey="help:sections.iphone.description"
+            components={[
+              <Anchor
+                href="https://apps.apple.com/us/app/v2raytun/id6476628951"
+                target="_blank"
+                rel="noopener noreferrer"
+                key={0}
+              />,
+              <strong key={1} />,
+              <em key={2} />,
+            ]}
+          />
         </Text>
-        <Accordion className={styles.accordion} title="Скриншоты v2RayTun">
+        <Accordion
+          className={styles.accordion}
+          title={t('sections.iphone.accordion.screenshots')}
+        >
           <Item value="screenshots">
-            <Control>Скриншоты</Control>
+            <Control>{t('sections.iphone.accordion.screenshots')}</Control>
             <Panel>
               {iphoneImages.map((el, i) => (
                 <Image src={el.image} alt={el.title} mt="sm" key={i} />
@@ -107,24 +111,29 @@ const HelpText: FC = () => {
           </Item>
         </Accordion>
       </section>
+
       <section id="Android">
-        <Title order={3}>Android</Title>
+        <Title order={3}>{t('sections.android.title')}</Title>
         <Text mt="sm">
-          Приложение
-          <Anchor
-            className={styles.link}
-            href="https://play.google.com/store/apps/details?id=app.hiddify.com"
-            target="_blank"
-            mr={0}
-          >
-            Hiddify
-          </Anchor>
-          . После чего скопировать конфиг и добавить его в приложение.{' '}
-          <em>Новый профиль → Добавить из буфер обмена → Подключиться</em>
+          <Trans
+            i18nKey="help:sections.android.description"
+            components={[
+              <Anchor
+                href="https://play.google.com/store/apps/details?id=app.hiddify.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                key={0}
+              />,
+              <strong key={1} />,
+            ]}
+          />
         </Text>
-        <Accordion className={styles.accordion} title="Посмотреть скриншоты">
+        <Accordion
+          className={styles.accordion}
+          title={t('sections.android.accordion.screenshots')}
+        >
           <Item value="screenshots">
-            <Control>Посмотреть скриншоты</Control>
+            <Control>{t('sections.android.accordion.screenshots')}</Control>
             <Panel>
               {androidImages.map((el, i) => (
                 <Image src={el.image} alt={el.title} mt="sm" key={i} />
@@ -133,58 +142,45 @@ const HelpText: FC = () => {
           </Item>
         </Accordion>
       </section>
+
       <section id="Windows">
-        <Title order={3}>Windows</Title>
+        <Title order={3}>{t('sections.windows.title')}</Title>
         <Text mt="sm">
-          <span className={styles.textBlock}>
-            Приложение Hiddify нужно запускать с правами администратора. Поэтому
-            рекомендуется скачать его с официальной страницы
-            <Anchor
-              className={styles.link}
-              href="https://github.com/hiddify/hiddify-next/releases"
-              target="_blank"
-            >
-              Github.
-            </Anchor>
-            Нужно открыть раздел <em>Assets</em> и скачать подходящую вам
-            версию. В нашем случае это <em>Hiddify-Windows-Setup-x64.exe.</em>{' '}
-            Также рекомендуется выбрать релиз с надписью <Badge>Latest</Badge>
-          </span>
-          <br />
-          <span className={styles.textBlock}>
-            Либо скачать с
-            <Anchor
-              className={styles.link}
-              href="https://apps.microsoft.com/detail/9pdfnl3qv2s5"
-              target="_blank"
-            >
-              Microsoft Store.
-            </Anchor>
-          </span>
+          <Trans
+            i18nKey="help:sections.windows.description"
+            components={[
+              <Anchor
+                href="https://github.com/hiddify/hiddify-next/releases"
+                target="_blank"
+                rel="noopener noreferrer"
+                key={0}
+              />,
+              <em key={1} />,
+              <Badge key={2} />,
+              <Anchor
+                href="https://apps.microsoft.com/detail/9pdfnl3qv2s5"
+                target="_blank"
+                rel="noopener noreferrer"
+                key={3}
+              />,
+            ]}
+          />
         </Text>
         <Title mb={10} order={3}>
-          Настройка приложения
+          {t('sections.windows.settings')}
         </Title>
         <Text className={styles.textBlock}>
-          Если вы скачали приложение с Github то у вас есть возможность
-          настроить автозапуск с правами администратора. Для этого нажмите на
-          ярлык правой кнопкой мыши и зайдите в{' '}
-          <em>
-            Свойства → Совместимость → Запуск от имени администратора →
-            Применить
-          </em>
-          .
-          <span className={styles.textBlock}>
-            После запуска приложения добавьте конфиг, преждевременно его
-            скопировав. Нажмите на иконку + в правом верхнем углу → Добавить из
-            буфер обмена. После чего выберите в правом верхнем углу выберите
-            иконку настроек. В появившемся меню измените с <em>Прокси</em> на{' '}
-            <em>VPN</em>. Теперь можете подключаться!
-          </span>
+          <Trans
+            i18nKey="help:sections.windows.settingsDescription"
+            components={[<em key={0} />, <strong key={2} />]}
+          />
         </Text>
-        <Accordion className={styles.accordion} title="Посмотреть скриншоты">
+        <Accordion
+          className={styles.accordion}
+          title={t('sections.windows.accordion.screenshots')}
+        >
           <Item value="screenshots">
-            <Control>Посмотреть скриншоты</Control>
+            <Control>{t('sections.windows.accordion.screenshots')}</Control>
             <Panel>
               {windowsImages.map((el, i) => (
                 <Image src={el.image} alt={el.title} mt="sm" key={i} />
@@ -193,11 +189,14 @@ const HelpText: FC = () => {
           </Item>
         </Accordion>
       </section>
+
       <section id="Linux">
-        <Title order={2}>Linux</Title>
+        <Title order={2}>{t('sections.linux.title')}</Title>
         <Text>
-          Аналогично подключению с Windows, только вместо <em>.exe</em> нужно
-          выбрать <em>Hiddify-Linux-x64.AppImage</em>
+          <Trans
+            i18nKey="help:sections.linux.description"
+            components={[<em key={0} />]}
+          />
         </Text>
       </section>
     </Container>
