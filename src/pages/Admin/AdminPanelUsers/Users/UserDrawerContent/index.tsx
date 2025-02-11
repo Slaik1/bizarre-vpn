cf1691a0ce45223d92357126d6d6ef860456560c
Slaik1 <marinakolmik@gmail.com>
2025-02-11 22:36:31 +0300

    wiop

--- src/pages/Admin/AdminPanelUsers/Users/UserDrawerContent/index.tsx
+++ src/pages/Admin/AdminPanelUsers/Users/UserDrawerContent/index.tsx
@@ -1,22 +1,32 @@
-import { FC } from 'react';
+import { Select } from '@mantine/core';
+import { FC, useState } from 'react';
 
 import { User } from '../../../../../ts/types/user';
 
 import cl from './UserDrawerContent.module.scss';
-import { Select } from '@mantine/core';
+import { UserRoles } from '../../../../../constants/user';
 
 interface UserDrawerContentProps {
   user: N<User>;
 }
 
 const UserDrawerContent: FC<UserDrawerContentProps> = ({ user }) => {
+  const [role, setRole] = useState(user?.role);
+  const [plan, setPlan] = useState(user?.role);
+
   if (!user) return;
 
-  const { createdAt, id, role, updatedAt, username } = user;
-
-  return (<div>
-		<Select placeholder='asd' value={role}/>
-	</div>);
+  return (
+    <div>
+      <Select onChange={setRole} label="Роль" value={role} data={UserRoles} />
+      <Select
+        onChange={(e) => console.log('e', e)}
+        label="Тарифный план"
+        value={plan}
+        data={['basic', 'stand']}
+      />
+    </div>
+  );
 };
 
 export default UserDrawerContent;
